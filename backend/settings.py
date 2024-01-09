26a7190cc10db1d58f23a5dca6f59f3539e74677
unknown <abdullahimohamedyusuf04@gmail.com>
2024-01-09 16:41:53 +0100

    adding postgresql

--- backend/settings.py
+++ backend/settings.py
@@ -105,14 +105,14 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",  # Adjust the path as needed
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'halal',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'ndcDluxfygty9Mu',
-        # 'HOST': 'localhost',
-        # 'PORT': '',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / "db.sqlite3",  # Adjust the path as needed
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'halal',
+         'USER': 'postgres',
+         'PASSWORD': 'ndcDluxfygty9Mu',
+         'HOST': 'localhost',
+         'PORT': '',
     }
 }
 
