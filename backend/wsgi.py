a0ab53db79d3ad1ebd5781a5f5dc83fecb3f1662
unknown <abdullahimohamedyusuf04@gmail.com>
2024-01-09 15:20:44 +0100

    return to sqlite

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,13 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-if os.environ.get('ENV') == 'production':
-    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 application = get_wsgi_application()
 
